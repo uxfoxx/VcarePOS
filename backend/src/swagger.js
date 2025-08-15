73e064637d0c7457ed17b0a99fbb1a9ab3115c27
NimeshaKahingala <nimesha.isholi94@gmail.com>
2025-08-15 13:33:51 -0500

    server url

--- backend/src/swagger.js
+++ backend/src/swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       { url: 'http://localhost:3000/api/' },
-      { url: 'http://68.183.182.142:3000/api/' }
+      { url: 'http://http://167.71.207.88/:3000/api/' }
     ],
   },
   apis: ['./src/routes/*.js'], // All route files

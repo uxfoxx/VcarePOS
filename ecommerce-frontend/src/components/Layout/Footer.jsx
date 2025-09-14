57eb568c1e86f9982876316579c0ac309d9f7a17
GGGod420 <90144831+GGGod420@users.noreply.github.com>
2025-09-14 13:35:28 +0530

    Fix handleLogout function in Footer component

--- ecommerce-frontend/src/components/Layout/Footer.jsx
+++ ecommerce-frontend/src/components/Layout/Footer.jsx
@@ -1,11 +1,15 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { logout } from '../../store/slices/authSlice';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 
-  
 const Footer = () => {
-  const { isAuthenticated, loading: authLoading } = useSelector(state => state.auth);
+  const dispatch = useDispatch();
+  const { isAuthenticated } = useSelector(state => state.auth);
+  
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+  
   return (
     <footer className="bg-gray-900 text-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">

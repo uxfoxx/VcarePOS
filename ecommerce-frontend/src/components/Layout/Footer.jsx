f8e9f51bfb33b55a0dfba3c3f1b96d11112ae75f
uxfoxx <158042245+uxfoxx@users.noreply.github.com>
2025-09-14 13:35:49 +0530

    gg

--- ecommerce-frontend/src/components/Layout/Footer.jsx
+++ ecommerce-frontend/src/components/Layout/Footer.jsx
@@ -1,7 +1,7 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { logout } from '../../store/slices/authSlice';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+
 
   
 const Footer = () => {

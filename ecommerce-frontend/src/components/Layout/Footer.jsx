2fe5cad485a73a99abb32c8b56323d1d95073439
GGGod420 <90144831+GGGod420@users.noreply.github.com>
2025-09-14 12:20:17 +0530

    Fix Footer.jsx closing tag and Redux imports

--- ecommerce-frontend/src/components/Layout/Footer.jsx
+++ ecommerce-frontend/src/components/Layout/Footer.jsx
@@ -1,6 +1,15 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { logout } from '../../store/slices/authSlice';
 import { Link } from 'react-router-dom';
 
 const Footer = () => {
+  const dispatch = useDispatch();
+  const { isAuthenticated, customer } = useSelector(state => state.auth);
+
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
   return (
     <footer className="bg-gray-900 text-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
@@ -139,8 +148,8 @@ const Footer = () => {
           </p>
         </div>
       </div>
-    </header>
+    </footer>
   );
 };
 
-export default Header;
+export default Footer;
